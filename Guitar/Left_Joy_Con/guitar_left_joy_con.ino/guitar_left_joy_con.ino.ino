0d45be7682bbd5316db7873e013d0d47e7703317
gitLabor8 <frank.gerlings@student.ru.nl>
2018-03-02 17:02:49 +0100

    LJC Guitar: logic finished

---
+++ Guitar/Left_Joy_Con/guitar_left_joy_con.ino/guitar_left_joy_con.ino.ino
@@ -0,0 +1,81 @@
+/*
+ * Left Joy Con of the Guitar concept: tapping bar
+ * 
+ * Reads input of joy stick. Sends bluetooth pulse to console when the joy stick is 'tapped'
+ * 
+ */
+
+#define DELAY 1000
+boolean ledOn = false;
+long lastTime = 0;
+
+int xPin = A1;
+int yPin = A2;
+int buttonPin = A3;
+
+int xPosition = 0;
+int yPosition = 0;
+int buttonState = 0;
+
+int oldX = 0;
+int oldY = 0;
+
+void setup() {
+  // Show that setup() is run, even though the execercise doesn't explicitly specify this
+  Serial.begin(9600);
+  //Serial.println("Program started!");
+
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
+  //activate pull-up resistor on the push-button pin
+  pinMode(buttonPin, INPUT_PULLUP); 
+  
+}
+
+void printJoystickState() {
+  Serial.print("X: ");
+  Serial.print(xPosition);
+  Serial.print(" | Y: ");
+  Serial.print(yPosition);
+  Serial.print(" | Button: ");
+  Serial.println(buttonState);
+  delay(DELAY);
+}
+
+void tapDetection(){
+  /*
+  
+  Serial.print(oldX);
+  Serial.print(" - ");
+  Serial.print(oldY);
+  Serial.println(yPosition);
+  Serial.println(oldX - xPosition);
+  */
+  if ((abs (oldX - xPosition) > 50) || (abs (oldY - yPosition)) > 50)
+    Serial.println("1");
+  oldX = xPosition;
+  oldY = yPosition;
+}
+
+void testPulse() {
+  Serial.println("1");
+  delay(DELAY);
+}
+
+
+void loop() {
+  xPosition = analogRead(xPin);
+  yPosition = analogRead(yPin);
+  buttonState = digitalRead(buttonPin);
+  
+  // TODO: tap op efficiente manier meten
+  //printJoystickState();
+
+  // For checking bluetooth
+  //testPulse();
+  // http://techwatch.keeward.com/geeks-and-nerds/how-to-configure-and-use-an-iteaduino-bt/
+
+  tapDetection();
+  delay(100);
+}
+
