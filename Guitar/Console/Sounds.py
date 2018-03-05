a7b243102206e3992761a981e7f8234d8e7e28e2
gitLabor8 <frankgerlings37@gmail.com>
2018-03-05 16:24:04 +0000

    Play Sounds

--- Guitar/Console/Sounds.py
+++ Guitar/Console/Sounds.py
@@ -1,40 +1,40 @@
-from playsound import playsound
+import pygame
+from pygame import mixer
 
 buttons = open("buttons.txt", "r")
+pygame.mixer.init(44100, -16,2,2048)
 
-button1, button2, button3 = buttons.readlines()
+button1, button2, button3, value0, value1 = buttons.readlines()
 
-if (button1 == "0" and button2 == "0" and button3 == "0"):
-    playsound('Sounds\Sound1')
+if (button1 == value0 and button2 == value0 and button3 == value0):
+    pygame.mixer.music.load('/home/pi/Desktop/NDL-TeamWintendo/Guitar/Console/Sounds/Sound1.mp3')
+    pygame.mixer.music.play()
 
-elif (button1 == "1" and button2 == "0" and button3 == "0"):
-    playsound('Sounds\Sound2')
+elif (button1 == value1 and button2 == value0 and button3 == value0):
+    pygame.mixer.music.load('/home/pi/Desktop/NDL-TeamWintendo/Guitar/Console/Sounds/Sound1.mp3')
+    pygame.mixer.music.play()
 
-elif (button1 == "0" and button2 == "1" and button3 == "0"):
-    playsound('Sounds\Sound3')
+elif (button1 == value0 and button2 == value1 and button3 == value0):
+    pygame.mixer.music.load('/home/pi/Desktop/NDL-TeamWintendo/Guitar/Console/Sounds/Sound1.mp3')
+    pygame.mixer.music.play()
 
-elif (button1 == "0" and button2 == "0" and button3 == "1"):
-    playsound('Sounds\Sound4')
+elif (button1 == value0 and button2 == value0 and button3 == value1):
+    ppygame.mixer.music.load('/home/pi/Desktop/NDL-TeamWintendo/Guitar/Console/Sounds/Sound1.mp3')
+    pygame.mixer.music.play()
 
-elif (button1 == "1" and button2 == "1" and button3 == "0"):
-    playsound('Sounds\Sound5')
+elif (button1 == value1 and button2 == value1 and button3 == value0):
+    pygame.mixer.music.load('/home/pi/Desktop/NDL-TeamWintendo/Guitar/Console/Sounds/Sound1.mp3')
+    pygame.mixer.music.play()
 
-elif (button1 == "1" and button2 == "0" and button3 == "1"):
-    playsound('Sounds\Sound6')
+elif (button1 == value1 and button2 == value0 and button3 == value1):
+    pygame.mixer.music.load('/home/pi/Desktop/NDL-TeamWintendo/Guitar/Console/Sounds/Sound1.mp3')
+    pygame.mixer.music.play()
 
-elif (button1 == "0" and button2 == "1" and button3 == "1"):
-    playsound('Sounds\Sound7')
+elif (button1 == value0 and button2 == value1 and button3 == value1):
+    pygame.mixer.music.load('/home/pi/Desktop/NDL-TeamWintendo/Guitar/Console/Sounds/Sound1.mp3')
+    pygame.mixer.music.play()playsound('Sounds\Sound7.mp3')
 
-elif (button1 == "1" and button2 == "1" and button3 == "1"):
-    playsound('Sounds\Sound8')
+elif (button1 == value1 and button2 == value1 and button3 == value1):
+    pygame.mixer.music.load('/home/pi/Desktop/NDL-TeamWintendo/Guitar/Console/Sounds/Sound1.mp3')
+    pygame.mixer.music.play()playsound('Sounds\Sound8.mp3')
 
-
-
-
-
-
-
-
-}
-
-#playsound()
--- Guitar/Right_Joy_Con/Continuous pictures.py
+++ Guitar/Right_Joy_Con/Continuous pictures.py
@@ -6,6 +6,6 @@ counter = 0
 while(True):
     print("Gonna take a picture")
     camera.resolution = (1280, 720)
-    camera.capture("/home/pi/Desktop/NDL-TeamWintendo/testimage{0}.jpg".format(counter))
+    camera.capture("/home/pi/shared/testimage{0}.jpg".format(counter))
     print("Took a picture")
     counter += 1
