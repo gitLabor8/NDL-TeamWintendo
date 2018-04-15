696c0f0ef2abdaa7ee9afa23aaed18c6606c5cdc
gitLabor8 <frank.gerlings@student.ru.nl>
2018-04-15 14:48:57 +0100

    Arduino: documentation finished

--- Guitar/Left_Joy_Con/guitar_left_joy_con/guitar_left_joy_con.ino
+++ Guitar/Left_Joy_Con/guitar_left_joy_con/guitar_left_joy_con.ino
@@ -1,8 +1,13 @@
 /*
  * Left Joy Con of the Guitar concept: tapping bar
  * 
- * Reads input of joy stick. Sends bluetooth pulse to console when the joy stick is 'tapped'
+ * Reads input of joy stick, detects 'tapping'
+ * Sends bluetooth pulse over serial bus to console upon 'tapping'
  * 
+ */
+
+/*
+ * Initialising variables
  */
 
 #define DELAY 1000
@@ -28,15 +33,33 @@ int yPosition = 0;
 int zPosition = 0;
 
 void setup() {
-  // Show that setup() is run, even though the execercise doesn't explicitly specify this
+  // Show that setup() is run, useful for debugging purposes
   Serial.begin(9600);
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
   pinMode(zPin, INPUT);
-  // Init hello-world pin
+  // Initialises the hello-world pin, used to check if the script is running in BT-mode
   pinMode(LED_BUILTIN, OUTPUT); 
 }
 
+/*
+ * Code that is executed in the loop
+ */
+
+void loop() {
+  // printJoystickState();
+  xPosition = analogRead(xPin);
+  yPosition = analogRead(yPin);
+  zPosition = digitalRead(zPin);
+
+  tapDetection();
+  delay(50);
+
+  // For checking bluetooth
+  // testPulse();
+}
+
+// Printing function for debugging purposes
 void printJoystickState() {
   Serial.print("X: ");
   Serial.print(analogRead(xPin));
@@ -47,7 +70,9 @@ void printJoystickState() {
   delay(DELAY);
 }
 
+// Changes 'tap' based on joy stick input
 void tapDetection(){
+  // Note: using the names of the enumeration in the if-condition 
   // Tap has been triggered and returned to initial positions afterwards
   if (tap == 1 &&
        (xPosition >= 490 && xPosition <= 510) &&
@@ -64,28 +89,20 @@ void tapDetection(){
       }
 }
 
+/*
+ * We tried to use bluetooth, the parts of the code especially designed for testing and debugging this connection are given
+ * Note that this code is *not* used in the final product
+ */
+ 
+// Blinks the built in LED every second. Used for checking 
 void test_Pulse() {
   Serial.println("1");
   test_builtinLedBlink();
   delay(DELAY);
 }
 
+// Blinks the builtin LED
 void test_builtinLedBlink(){
     pulseLedOn = not pulseLedOn;
     digitalWrite(LED_BUILTIN, pulseLedOn);
 }
-
-void loop() {
-  // printJoystickState();
-  xPosition = analogRead(xPin);
-  yPosition = analogRead(yPin);
-  zPosition = digitalRead(zPin);
-
-  // For checking bluetooth
-  // testPulse();
-  // http://techwatch.keeward.com/geeks-and-nerds/how-to-configure-and-use-an-iteaduino-bt/
-
-  tapDetection();
-  delay(50);
-}
-
