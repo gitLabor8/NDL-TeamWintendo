2114e070713a28d010dcf7226227a122051101dd
Justin-Reniers <justin-reniers@hotmail.com>
2018-06-11 14:45:46 +0200

    Update van server files

--- Guitar/Console/Website/server.js
+++ Guitar/Console/Website/server.js
@@ -1,8 +1,6 @@
-
-
 var express = require("express");
-var app = express();
-var http = require("http");
+var app = require('express')();
+var http = require("http").Server(app);
 var path = require('path');
 
 app.set('view engine', 'ejs');
@@ -13,16 +11,38 @@ app.get('/', function (req, res) {
 app.use(express.static(path.join(__dirname, 'views')));
 
 var tuple = '';
-var http_server = http.createServer(app).listen(3000)
-var http_io = require("socket.io")(http_server);
-http_io.on("connection", function(httpsocket) {
-    httpsocket.on('python-message', function(fromPython) {
-        httpsocket.broadcast.emit('message', fromPython);
+var io = require("socket.io")(http);
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  io.emit('server-message', 'hello, server');
+
+  socket.on('python-message', function(fromPython) {
+    io.emit('server-message', fromPython);
+  })
+
+  socket.on('message', function(message) {
+    console.log(message);
+  })
+});
+http.listen(3000);
+
+//var http_server = http.createServer(app).listen(3000);
+
+/** var io = require("socket.io")(http);//(http_server);
+io.on("connection", function(socket) {
+
+    console.log('Python connected');
+    socket.on('python-message', function(fromPython) {
+        io.emit('python-message', fromPython);
+        socket.emit('python-message', fromPython);
         console.log('message', fromPython);
         tuple = fromPython;
-        console.log(tuple)
-        app.use(function(err, req, res, next) {
-          res.send(tuple);
-        });
+        console.log(tuple);
+        socket.send(tuple);
     });
 });
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+}); **/
